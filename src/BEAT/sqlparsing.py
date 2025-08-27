e04db2fa8e47432074e93d1ad59edf37660876a6
Cremers K.M.P. (HG) <k.m.p.cremers@lumc.nl>
2025-08-27 15:18:24 +0200

    documentation

--- src/BEAT/sqlparsing.py
+++ src/BEAT/sqlparsing.py
@@ -17,7 +17,7 @@ from os import getenv
 
 
 
-class SQLparser(Converter):
+class SQLParser(Converter):
 
     def __init__(self, config, client, class_map=None, debug=False):
         super().__init__(class_map, debug)
@@ -26,6 +26,17 @@ class SQLparser(Converter):
         self.VCARD = Namespace("http://www.w3.org/2006/vcard/ns#")
 
     def dataset_sql_query(self, cursor, dataset_id):
+        """SQL dataset query generator that should result in
+         the metadata related to the dataset with dataset_id
+
+        :param cursor: connection to a mssql database
+        :type cursor: Cursor object
+        :param dataset_id: dataset SQL index
+        :type dataset_id: str
+        :return: output of mssql database
+        :rtype: Cursor object
+    
+        """
 
         def dataset_sql_query_constructor(table_name, dataset_id) -> str:
             dataset_query = "select * from " + str(table_name) + " where identifier = '" + str(dataset_id) + "';"
@@ -37,7 +48,15 @@ class SQLparser(Converter):
         return cursor
 
     def catalog_sql_query(self, cursor):
+        """SQL catalog query generator that should result in
+         the metadata related to the catalog or database
 
+        :param cursor: connection to a mssql database
+        :type cursor: Cursor object
+        :return: output of mssql database
+        :rtype: Cursor object
+    
+        """
         def catalog_sql_query_constructor(table_name) -> str:
             catalog_query = "select * from "  + str(table_name) + ";"
             return catalog_query
@@ -47,26 +66,46 @@ class SQLparser(Converter):
         return cursor
 
     def connect_to_sql(self):
+        """Connect to a mssql database and build an interface for it.
+        Database should exists in network
+
+        :return: Interface 
+        :rtype: Cursor object.
+
+        """
         conn = connect(server=self.config["SQL"]["server_name"],user=self.config["SQL"]["username"],password=keyring.get_password(service_name=self.config["SQL"]["keyring_service"], username=self.config["SQL"]["username"]), database=self.config["SQL"]["database_name"],tds_version="7.4")
         cursor = conn.cursor(as_dict=True)
         return cursor
     
     def parse(self):
+        # get catalog information:
         cursor = self.connect_to_sql()
         catalog_table = self.catalog_sql_query(cursor)
+
         for catalog in catalog_table:
+            # transform catalog metadata:
             self.sempyro_catalog(pd.Series(catalog), self.graph, url=self.client.URL + "/new")
+            # replace subject id with url needed to upload new data to FDP:
             self.subject_replace(self.client.PURL + "new", BNode("Catalog"), DCAT.Catalog, self.graph)
+            #upload catalog
             catalog_purl = client.upload_resource(self.graph, self.client.URL, resource_type=DCAT.Catalog)
+            # get associated dataset sql index ids
             for dataset_id in catalog["datasets"].split(","):
+                # get metadata for dataset
                 dataset_metadata = self.dataset_sql_query(cursor, dataset_id).__next__()
+                # add dataset to graph:
                 self.sempyro_dataset(pd.Series(dataset_metadata), self.graph, self.client.PURL)
+            # get a list of all datasets in the graph:
             dataset_list = self.get_dataset_nodes(self.graph)
             for node_id in dataset_list:
+                # extract dataset specific information
                 dataset_graph = self.graph.cbd(node_id[0])
                 self.subject_replace(self.client.PURL + "new", node_id[0], DCAT.Dataset, dataset_graph)
+                # remove dataset metadata from graph
                 dataset_graph.remove((BNode(node_id[0]), None, None))
+                # link dataset to catalog
                 client.link_resource(dataset_graph, catalog_purl, DCAT.Dataset)
+                # upload dataset metadata
                 client.upload_resource(dataset_graph, catalog_purl, resource_type=DCAT.Dataset, resource_name="Dataset")
         
     def dataservice_rdf(self, metadata, graph):
@@ -83,7 +122,7 @@ class SQLparser(Converter):
 
 
 if __name__ == "__main__":
-    ## TODO should be done in the main:
+    ## TODO should be done in the main.py:
     conf_path = getenv("CONF_PATH", default="config/configuration.yaml")
 
     # get default values from config file
@@ -100,7 +139,7 @@ if __name__ == "__main__":
         return URL, PURL, client
 
     URL, PURL, client = connect_client()
-    parser = SQLparser(config, client)
+    parser = SQLParser(config, client)
     parser.parse()
 
     
