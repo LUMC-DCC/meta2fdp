c90f631641f14f1ba33c3392e41317c7fa1d5e4c
Cremers K.M.P. (HG) <k.m.p.cremers@lumc.nl>
2026-02-16 17:13:22 +0100

    Working secrets system in ephemeral test FDP and FDPclient module. Updated test SHACL's to comply with HRI v2.0.2. Wrote a full pipeline that tests all the features for csv

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,4 @@ BASE_DIR = pathlib.Path(__file__).resolve().parent
 
 # facilitate running tests from command line using `python -m unittest`
 sys.path.append(str(BASE_DIR.parent / 'samplenavigator2fdp'))
+sys.path.append(str(BASE_DIR.parent))
