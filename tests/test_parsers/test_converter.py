8c5724d973e2d62faad659f28703b2f825f822ec
niehues <1405356+niehues@users.noreply.github.com>
2026-03-03 16:27:41 +0100

    remove unused tests

--- tests/test_parsers/test_converter.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-import meta2fdp
-
-
-class ConverterTests(TestCase):
-    def test_vcard_rdf(self):
-        Converter = meta2fdp.parsers.hriconverter()
-        # TODO Series object with dataset metadata.
-        Converter.vcard_rdf()
