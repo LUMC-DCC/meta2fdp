445431e8a668576e5cd015e9a1695aa33f297f57
Cremers <k.m.p.cremers@lumc.nl>
2026-05-05 13:56:45 +0200

    add secrets provider as a component of a publishmetadatapipeline

--- src/meta2fdp/pipeline/publish_metadata.py
+++ src/meta2fdp/pipeline/publish_metadata.py
@@ -2,14 +2,21 @@
 
 from meta2fdp.pipeline.base import Pipeline
 from meta2fdp.config.schema import SchemaConfig
+from meta2fdp.secrets.base import SecretsProvider
 
 
 class PublishMetadataPipeline(Pipeline):
     """Pipeline for publishing metadata to the FDP server. This pipeline includes components for authentication, metadata extraction, RDF conversion, and posting to the FDP server."""
 
-    def __init__(self, schema_config: SchemaConfig, registries: dict):
+    def __init__(
+        self,
+        schema_config: SchemaConfig,
+        registries: dict,
+        secrets_provider: SecretsProvider,
+    ):
         """Initialize the PublishMetadataPipeline with the given schema configuration. The schema configuration specifies which metadata schema to use for RDF conversion.
         Parameters:
+        secrets_provider (SecretsProvider): The secrets provider to use for retrieving sensitive information such as API keys and credentials.
         schema_config (SchemaConfig): The configuration for the metadata schema to use.
         connector_config (ConnectorConfig): The configuration for the data connector to use for metadata extraction.
         extractor_config (ExtractorConfig): The configuration for the metadata extractor to use for metadata extraction.
@@ -20,7 +27,7 @@ class PublishMetadataPipeline(Pipeline):
         self.schema_config = schema_config
         # TODO: add other configs: connector_config, extractor_config, fdp_config
         self.registries = registries
-
+        self.secrets_provider = secrets_provider
         self.schema_module = self._resolve_schema()
 
     def _resolve_schema(self):
