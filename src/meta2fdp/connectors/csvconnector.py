381372a7559348d4b8c138b597bf20d2e9654f4e
Cremers K.M.P. (HG) <k.m.p.cremers@lumc.nl>
2026-05-13 11:33:46 +0200

    CSV connector module implementation for catalogs and datasets, we currently assume that all the subclasses (Agents like publisher and creator,Vcards like contactpoint) of these resources are also described in their associated files

---
+++ src/meta2fdp/connectors/csvconnector.py
@@ -0,0 +1,46 @@
+"""Connector for reading metadata from CSV files."""
+
+from os import PathLike
+from pathlib import Path
+from typing import Union
+from meta2fdp.config.connector.csvconnector import CsvConnectorConfig
+import pandas as pd
+
+
+class CSVConnector:
+    """Connector for reading metadata from CSV files. This connector is designed to read metadata from CSV files and convert it into a format that can be used by the rest of the meta2fdp pipeline. The connector uses the configuration parameters specified in the CsvConnectorConfig class to determine the file paths for the catalog and dataset metadata, and it implements methods to read these files and return the metadata as pandas DataFrames. The connector also includes error handling to ensure that the specified file paths exist and that the required parameters are provided in the configuration."""
+
+    def __init__(self, config: CsvConnectorConfig):
+        self.config = config
+
+    def _resolve_path(self, p: Union[str, PathLike, Path]) -> Path:
+        """
+        Resolve and validate a filesystem path.
+
+        :param p: Path-like object or string to resolve.
+        :type p: str or os.PathLike or pathlib.Path
+        :returns: Resolved pathlib.Path
+        :rtype: pathlib.Path
+        :raises FileNotFoundError: If the resolved path does not exist.
+        """
+        p = Path(p)
+        p = p.expanduser().resolve(strict=False)
+        if not p.exists():
+            raise FileNotFoundError(f"File not found: {p}")
+        return p
+
+    def read_catalog(self) -> pd.DataFrame:
+        """Read the catalog metadata from the specified CSV file and return it as a pandas DataFrame."""
+        catalog_path = self.config.catalog_input_file
+        resolved_path = self._resolve_path(catalog_path)
+        return pd.read_csv(
+            resolved_path, sep=self.config.separator, header=self.config.header
+        )
+
+    def read_dataset(self) -> pd.DataFrame:
+        """Read the dataset metadata from the specified CSV file and return it as a pandas DataFrame."""
+        dataset_path = self.config.dataset_input_file
+        resolved_path = self._resolve_path(dataset_path)
+        return pd.read_csv(
+            resolved_path, sep=self.config.separator, header=self.config.header
+        )
