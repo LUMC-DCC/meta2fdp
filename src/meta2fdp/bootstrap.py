37e58c943f775fd814c97948b34e3f2338df6035
Cremers K.M.P. (HG) <k.m.p.cremers@lumc.nl>
2026-05-22 10:22:34 +0200

    Hack implementation of bootstrap to see if other components work before working on quality of life components

--- src/meta2fdp/bootstrap.py
+++ src/meta2fdp/bootstrap.py
@@ -1,8 +1,43 @@
-import meta2fdp.models.HRIcore.v2 as HRIcore_v2
-from meta2fdp.models.registry import ModelRegistry
+import meta2fdp.transformers.HRIcore.v2 as HRIcore_v2
+from meta2fdp.connectors.csvconnector import CSVConnector
+from meta2fdp.extractors.dfextractor import DFExtractor
+from meta2fdp.fdp.fdpclient import FDPClient
+from meta2fdp.secrets.environmentprovider import EnvSecretsProvider
+from meta2fdp.secrets.keyringprovider import KeyringSecretsProvider
+from meta2fdp.transformers.registry import TransformerRegistry
+
+
+def register_transformers():
+    registry = TransformerRegistry()
+    registry.register("HRIcore", "v2", HRIcore_v2)
+    return registry
+
+
+def register_connectors():
+    return {"csv": CSVConnector}
+
+
+def register_extractors():
+    return {"df": DFExtractor}
+
+
+def register_fdps():
+    return {"FDPClient": FDPClient}
+
+
+def register_secrets():
+    return {"env": EnvSecretsProvider, "keyring": KeyringSecretsProvider}
+
+
+def register_modules():
+    return {
+        "models": register_transformers(),
+        "connectors": register_connectors(),
+        "extractors": register_extractors(),
+        "fdp_clients": register_fdps(),
+        "secrets": register_secrets(),
+    }
 
 
 def register_models():
-    registry = ModelRegistry()
-    registry.register("HRIcore", "v2", HRIcore_v2)
-    return registry
+    return register_transformers()
